11747cc3133a910d7fc3a2557e8914c83c385e2d
janascher <janainascher@protonmail.com>
2024-06-22 15:10:58 -0300

    feat: config BDD e Cucumber

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
+
+module.exports = defineConfig({
+    e2e: {
+        specPattern: '**/*.feature', // extensão dos arquivos
+        setupNodeEvents(on, config) {
+            addCucumberPreprocessorPlugin(on, config);
+            on(
+                'file:preprocessor',
+                createBundler({
+                    plugins: [createEsbuildPlugin(config)],
+                })
+            );
+
+            return config;
+        },
+    },
+});
